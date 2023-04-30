37d68369727c7c9dffd2b80a543e14593ab8a819
gabigarciam <125141315+gabigarciam@users.noreply.github.com>
2023-04-30 12:28:07 +0200

    SO-V4

---
+++ SO/Prácticas/VersionesAnteriores/V4/V4-7/V4/Clock.c
@@ -0,0 +1,19 @@
+#include "Clock.h"
+#include "Processor.h"
+#include "ComputerSystemBase.h"
+
+int tics=0;
+
+void Clock_Update() {
+	tics++;
+	if(tics%intervalBetweenInterrupts == 0){
+		Processor_RaiseInterrupt(CLOCKINT_BIT);
+	}
+    // ComputerSystem_DebugMessage(97,CLOCK,tics); // Coment in V2-StudentsCode
+}
+
+
+int Clock_GetTime() {
+
+	return tics;
+}
